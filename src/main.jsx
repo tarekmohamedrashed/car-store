fbda67c7f84d68f9103e538471e6c6fd9d080d40
tarekmohamedrashed <163614617+tarekmohamedrashed@users.noreply.github.com>
2024-12-10 23:33:54 +0200

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
